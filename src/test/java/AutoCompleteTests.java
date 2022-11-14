d5811ac527df336afb5a97e3a8111439c763ef6b
Anton Barabanov <anton.barabanov95@gmail.com>
2022-11-14 19:52:11 +0300

    Create AutoCompleteTests add base for autotests

---
+++ src/test/java/AutoCompleteTests.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class AutoCompleteTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillAutoCompleteForm() {
+        open("/auto-complete");
+    }
+}
